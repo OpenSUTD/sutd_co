6b8b58cd9690d90ebdd18bec47cd6c91bd5b7b59
tjianhui <59618049+tjianhui@users.noreply.github.com>
2020-05-17 00:54:56 +0800

    Tidying up
    1. Moved everything I did in min back to source, then minified again
    2. Collated pages into folder
    3. Facebook embedding instructions

--- assets/js/stylish-portfolio.js
+++ assets/js/stylish-portfolio.js
@@ -1,14 +1,27 @@
+/*!
+This is where you'll write your scripts but in jQuery.
+I think it's probably not good prog practice to have both jQuery and javascript (inside index.html) even though afaik both are the same,
+  but for organising, I'm placing general page behaviour here, while the index.html javascripts are purely for the highlights modals
+ */
+
 (function ($) {
   "use strict"; // Start of use strict
 
-  // Closes the sidebar menu
+  // this function controls the sidebar menu button
   $(".menu-toggle").click(function (e) {
     e.preventDefault();
     $("#sidebar-wrapper").toggleClass("active");
+    //if active, it means the sidebar menu is open
     $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass(
       "fa-bars fa-times"
     );
     $(this).toggleClass("active");
+    //if sidebar menu is open, display the black veil. else, close it.
+    if ($("#sidebar-wrapper").attr("class") == "active") {
+      $("#menuModal").css("display", "block");
+    } else {
+      $("#menuModal").css("display", "none");
+    }
   });
 
   // Smooth scrolling using jQuery easing
@@ -33,16 +46,17 @@
     }
   });
 
-  // Closes responsive menu when a scroll trigger link is clicked
+  // Closes sidebar menu when a scroll trigger link is clicked
   $("#sidebar-wrapper .js-scroll-trigger").click(function () {
     $("#sidebar-wrapper").removeClass("active");
     $(".menu-toggle").removeClass("active");
     $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass(
       "fa-bars fa-times"
     );
+    $("#menuModal").css("display", "none");
   });
 
-  // Scroll to top button appear
+  // this is the scroll-to-top button on the bottom right-hand corner
   $(document).scroll(function () {
     var scrollDistance = $(this).scrollTop();
     if (scrollDistance > 100) {
@@ -51,4 +65,33 @@
       $(".scroll-to-top").fadeOut();
     }
   });
+
+  //this is written by me. added the functionality where if you click outside of the sidebar menu, it will close as well
+  $("#menuModal").click(function (e) {
+    var hold = $("#sidebar-wrapper");
+    if (hold.attr("class") == "active" && !hold.is(e.target)) {
+      $("#sidebar-wrapper").removeClass("active");
+      $(".menu-toggle").removeClass("active");
+      $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass(
+        "fa-bars fa-times"
+      );
+      $("#menuModal").css("display", "none");
+      /*hiding the black veil as well*/
+    }
+  });
 })(jQuery); // End of use strict
+
+var onMapMouseleaveHandler = function (e) {
+  var a = $(this);
+  a.on("click", onMapClickHandler);
+  a.off("mouseleave", onMapMouseleaveHandler);
+  a.find("iframe").css("pointer-events", "none");
+};
+
+var onMapClickHandler = function (e) {
+  var a = $(this);
+  a.off("click", onMapClickHandler);
+  a.find("iframe").css("pointer-events", "auto");
+  a.on("mouseleave", onMapMouseleaveHandler);
+};
+$(".map").on("click", onMapClickHandler);
